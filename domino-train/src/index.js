9cbd01868375583e242c8fbc64cddc498f1ee52a
jacobsthompson <jacob.s.thompson@icloud.com>
2026-03-18 11:41:58 -0700

    V1.2: Added Vercel Analytics
    Components Done:
    - Grid Placement and Value Storage
    - Domino Randomized Generation
    - Domino Holder
    - Domino Pickup
    - Domino Dragging Movement
    - Domino Placement
    - Domino Rotation
    - Basic Layout and Design
    - Scoring
    - Validation (BFS)
    - Path Correction
    - Domino Coloring
    - Implemented Square Rule (No 2x2 adjacent dominos)
    - Fixed Sandwiching Bug
    - Added Auto Validation
    - Added SVG Logo
    - Added Board Cell Coloring
    - Improved Pip Coloring
    - Improved Domino Holder to fit under grid
    - Created Animated Score Bar
    - Added Sound Effects for: Pickup, Placement, Rotation, Clear, and Scoring
    - Updated Clear Board Icon
    - *MOST IMPORTANT* Generate 100% Winnable Boards
    - Placed Clear Board Button Under Domino Holder
    - Added Dynamic Grid Resizing
    - Started Modals (How to play)
    - Created "Tutorial Domino" for teaching controls
    - Added Mobile Domino Dragging
    - Added Mobile Rotation with Twist Motion
    - Added Mobile Rotation Midpoint Offset for Visual Clarity
    - Improved Validity UI (Show Partial Complete Path as Green, Rest Red)
    - Board Glows on Win
    - ** ValidateBoard Finished with polished square rule
    - ** Shows current valid path(s) while placing dominos
    - ** Shows invalid dominos without path being completed
    - Added new Start Screen (Title, Date, Start Button, How To Play Button)
    - Fixed glitch when rotating before movement causing offset issues by setting currentMousePos on mouse down.
    - Finalized Tutorial Text
    - Added Fake/Example Dominos to demonstrate rules
    - Animation on winning
    - Created Win Modal
    - Added Stats!
    - Added Stats Modal
    - Added Header with New Title, "How To Play", "Stats", and "More Projects (Link TBD)" Buttons
    - Changed index.html background color (removed flashing white light on reload)
    - Pre-rendered svgs for optimal loading
    - **DAILY GAME!**
    - Domino Generation Based on seed
    - Winning Domino Solve Saved and shows up on refresh
    - Grid State and Domino Position States now Saved
    - Endless Mode Added!
    - DominoHolder Regenerates new set of dominos from new Math.random() solution
    - Snap Scoreboard Animation to Full Success on Refresh and Empty on "Play Endless"
    - Core Project Done!
    - Triumphant Sound on Win
    - Added "New Game" Button during endless mode
    - Increased Grid size to 8x8 and Domino Start Count to 16
    - Fixed starting tile loose connection bug
    - Organized Files and seperated Stylesheets.
    - Added Light/Dark Mode Icons
    - Added UpdateStreak
    - Added OpenTutorial on First Game
    - Added Undo and Redo Buttons
    - Added DominoHistory and GridHistory to keep track of all domino positions
    - Added the ability to change board state on undo and redo
    - When going back in the undo, and doing a new move, sliceHistory erases the indexes ahead of the current position and adds the new move.
    - History resets on endless mode games
    - Smaller Phone Screen Resize
    - Improved Tutorial
    - Timer Implemented and Saved in Local Storage
    - Best Time Saved and Shown in Stats
    - Made Tutorial Domino More Apparent
    - Added newGameModal, a confirmation for starting new games
    - Added Vercel Analytics
    Bonus Feature(s):
    - Add blackout spots to grid
    - Easy Mode

--- domino-train/src/index.js
+++ domino-train/src/index.js
@@ -3,12 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-import { Analytics } from "@vercel/analytics/next"
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
-    <Analytics/>
   </React.StrictMode>
 );
